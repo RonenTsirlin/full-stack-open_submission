a45341005790f6682cb96439236cada6ad4822ff
Ronen Tsirlin <ronentz123@gmail.com>
2025-05-22 00:21:03 +0300

    Added part3-2.

--- part3/lesson/index.js
+++ part3/lesson/index.js
@@ -2,6 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
 let notes = [
   {
     id: "1",
@@ -25,8 +43,6 @@ const generateId = () => {
     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
   return String(maxId + 1);
 };
-
-app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
