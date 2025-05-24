2cb3e7af04c801089afe4430d291f0a33c47590b
Ronen Tsirlin <ronentz123@gmail.com>
2025-05-24 19:31:53 +0300

    Removed cors.

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -3,9 +3,6 @@ const app = express();
 app.use(express.static("dist"));
 app.use(express.json());
 
-const cors = require("cors");
-app.use(cors());
-
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
