e3925462301689e61d5286b52418ede8765c88b2
Ronen Tsirlin <ronentz123@gmail.com>
2025-05-26 13:33:59 +0300

    Finished exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("Give password as an argument.");
+  process.exit(1);
+}
+
+let password = "";
+let inputName = "";
+let inputNumber = "";
+
+if (process.argv.length > 3 && process.argv.length < 6) {
+  password = process.argv[2];
+  inputName = process.argv[3];
+  inputNumber = process.argv[4];
+} else {
+  password = process.argv[2];
+}
+
+const url = `mongodb+srv://fullstack:${password}@phonebook.da6jt8q.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Phonebook`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (inputName && inputNumber) {
+  const person = new Person({
+    name: inputName,
+    number: inputNumber,
+  });
+
+  person.save().then((result) => {
+    console.log(`Added ${result.name} number ${result.number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 3) {
+  Person.find({}).then((people) => {
+    console.log("phonebook:");
+    people.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
