7002ba63f523771eae33b4efeffa41ed46319569
Ronen Tsirlin <ronentz123@gmail.com>
2025-05-26 16:01:27 +0300

    Finished part 3.14

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,12 +1,8 @@
+require("dotenv").config();
+const Note = require("./models/note");
 const express = require("express");
-const app = express();
-app.use(express.static("dist"));
-app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const app = express();
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -15,7 +11,10 @@ const requestLogger = (request, response, next) => {
   console.log("---");
   next();
 };
+
 app.use(requestLogger);
+app.use(express.static("dist"));
+app.use(express.json());
 
 let notes = [
   {
@@ -40,43 +39,32 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const note = notes.find((note) => note.id === id);
-
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).send("404 Note does not exist");
-  }
+  });
 });
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+    return response.status(400).json({ error: "content missing" });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -90,3 +78,8 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,14 +1,13 @@
+require("dotenv").config();
+const Person = require("./models/person");
+const morgan = require("morgan");
 const express = require("express");
+
 const app = express();
+
 app.use(express.static("dist"));
 app.use(express.json());
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-const morgan = require("morgan");
 morgan.token("body", (request) => {
   return JSON.stringify(request.body);
 });
@@ -17,80 +16,37 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let people = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Backend Phonebook App</h1>");
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(people);
+  Person.find({}).then((people) => {
+    response.json(people);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  const person = people.find((person) => person.id === id);
-
-  if (person) {
+  Person.findById(request.params.id).then((person) => {
     response.json(person);
-  } else {
-    response.status(404).send("404 Person does not exist");
-  }
+  });
 });
 
-const generateID = () => {
-  return Math.floor(Math.random() * 100_000_000) + 1;
-};
-
 app.post("/api/persons", (request, response) => {
-  body = request.body;
+  const body = request.body;
 
   if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: "name or number missing",
-    });
+    return response.status(400).json({ error: "name or number is missing" });
   }
 
-  if (people.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: "name must be unique",
-    });
-  }
-
-  do {
-    generatedID = generateID();
-  } while (people.find((person) => person.id === generatedID));
-
-  const person = {
-    id: generatedID,
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  people = people.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -102,14 +58,20 @@ app.delete("/api/persons/:id", (request, response) => {
 
 app.get("/info", (request, response) => {
   const timeReceived = new Date().toString();
-  response.send(`
+  Person.find({}).then((people) => {
+    response.send(`
     <p>Phonebook has info for ${people.length} people</p>
-    <p>${timeReceived}</p>
-    
+    <p>${timeReceived}</p>  
 `);
+  });
 });
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
