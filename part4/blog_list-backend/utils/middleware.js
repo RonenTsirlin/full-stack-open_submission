d22abab2fa77a4662c6d200c12623cc86f6cb605
Ronen Tsirlin <ronentz123@gmail.com>
2025-05-30 17:57:58 +0300

    Finished blog List, step 2

--- part4/blog_list-backend/utils/middleware.js
+++ part4/blog_list-backend/utils/middleware.js
@@ -3,7 +3,11 @@ const logger = require("./logger");
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
   logger.info("Path:  ", request.path);
-  logger.info("Body:  ", request.body);
+
+  if (request.method !== "GET") {
+    logger.info("Body:  ", request.body);
+  }
+
   logger.info("---");
   next();
 };
